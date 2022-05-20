9ce81ebdca016f5f6b054aa9807c772975beefbb
flames <rrahulao2000007@gmail.com>
2022-05-20 20:52:07 +0530

    needs changes

---
+++ Voting_System_Blockchain/client/src/component/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => (
+  <>
+    <div className="footer-block"></div>
+    <div className="footer">
+      <div className="footer-container">
+        <p>
+          Election
+        </p>
+      </div>
+    </div>
+  </>
+);
+
+export default Footer;
---
+++ Voting_System_Blockchain/client/src/component/NotInit.js
@@ -0,0 +1,14 @@
+// Node module
+import React from "react";
+
+const NotInit = () => {
+  // Component: Displaying election not initialize message.
+  return (
+    <div className="container-item info">
+      <center>
+        <h3>There are no ongoing elections.</h3>
+      </center>
+    </div>
+  );
+};
+export default NotInit;
